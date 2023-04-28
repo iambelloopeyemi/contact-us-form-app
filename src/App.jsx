5cdab5b22df0ec4e745bf30f3524f9571c6ac1e0
iambelloopeyemi <opeyemiemmanuelbello@gmail.com>
2023-04-28 10:51:36 +0100

    set up the custom form branch project folder and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,3 @@
-import ContactUsForm from "./components/ContactUsForm";
-import FormHeader from "./components/FormHeader";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-
-function App() {
-  
-  return (
-    <Container component="main" fixed>
-      <Box component="main" className="container" >
-        <FormHeader />
-        <ContactUsForm />
-      </Box>
-    </Container>
-  );
+export default function App() {
+  return <div></div>;
 }
-
-export default App;
